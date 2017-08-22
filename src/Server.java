f3b223f8b127685e0111e0d9c97f6c610e1bff83
Ross Newby <rossnewby@hotmail.co.uk>
2017-08-22 13:09:09 +0100

    Server Interface / Implementation foundations

--- src/Server.java
+++ src/Server.java
@@ -2,40 +2,29 @@ import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
 /**
  * @Author Ross Newby
  */
 public class Server {
 
-    private final String USER_AGENT = "Mozilla/5.0";
-
-    private String cookie = "cosign-https-ckan.lancaster.ac.uk";
-    private String cookiename = "VdV48bc+gJh8i4NRtvZx95OTVkyGCtoaF-TSaIOm7lz-9v3kxvvlqodDKiUKwvaNpMiuLWkgerUGySpMETKd6SqtyU9bKpcZ6pS5bydnVKAcgdUsHWlDA1dWNJ2l/1503307766";
-
     public Server(){
+
+        /*Bind the remote object to the RMI registry*/
         try {
-            // http://www.baeldung.com/java-http-request
-            URL url = new URL("https://ckan.lancaster.ac.uk/api/3/action/package_show?id=planonmetadata");
-            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Cookie", cookiename+"="+cookie);
+            /*Start the RMI registry on port 1099*/
+            Runtime.getRuntime().exec("rmiregistry 1099");
+            LocateRegistry.createRegistry(1099);
 
-            // Problem with security Certificate needs fixing
-//            BufferedReader in = new BufferedReader(
-//                    new InputStreamReader(con.getInputStream()));
-//            String inputLine;
-//            StringBuffer response = new StringBuffer();
-//            while ((inputLine = in.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//            System.out.println(response.toString()); //print result
-
-//            in.close();
-            con.disconnect();
+            /*Bind the ServerImpl class to the RMI registry for remote calls by clients*/
+            ServerInterface c = new ServerImpl();
+            Naming.rebind("rmi://localhost:1099/QualityService", c);
         }
-        catch (Exception e){
-            e.printStackTrace();
+        catch (Exception e) {
+            System.out.println("RMI Binding Error: " + e);
+            // e.printStackTrace();
         }
     }
 
--- src/ServerImpl.java
+++ src/ServerImpl.java
@@ -1,11 +1,47 @@
-import java.rmi.RemoteException;
-
 /**
  * Contains the methods which can be remotely invoked
  *
  * @Author Ross Newby
  */
-public class ServerImpl {
+import javax.net.ssl.HttpsURLConnection;
+import java.net.URL;
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServerImpl extends UnicastRemoteObject implements ServerInterface{
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    private String cookie = "cosign-https-ckan.lancaster.ac.uk";
+    private String cookiename = "VdV48bc+gJh8i4NRtvZx95OTVkyGCtoaF-TSaIOm7lz-9v3kxvvlqodDKiUKwvaNpMiuLWkgerUGySpMETKd6SqtyU9bKpcZ6pS5bydnVKAcgdUsHWlDA1dWNJ2l/1503307766";
+
+    public ServerImpl() throws RemoteException{
+
+        try {
+            // http://www.baeldung.com/java-http-request
+            URL url = new URL("https://ckan.lancaster.ac.uk/api/3/action/package_show?id=planonmetadata");
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Cookie", cookiename+"="+cookie);
+
+            // Problem with security Certificate needs fixing
+//            BufferedReader in = new BufferedReader(
+//                    new InputStreamReader(con.getInputStream()));
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//            while ((inputLine = in.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            System.out.println(response.toString()); //print result
+
+//            in.close();
+            con.disconnect();
+        }
+        catch (Exception e){
+            System.out.println("CKAN Connection Error: " + e);
+            // e.printStackTrace();
+        }
+    }
 
     //
     public int method () throws RemoteException{
